49bae603865dda7e7e7bfd20acd10fbacdbaf855
Rockingham-Web-Services1 <alessandra@rockinghamwebservices.com>
2024-04-05 09:38:43 +0800

    new changes

--- contact_486/contact_486_modified/contact_486_v2.svelte
+++ contact_486/contact_486_modified/contact_486_v2.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let p_0, h2_0, h3_0, a_0, href_0, a_1, href_1, name_0, placeholder_0, type_0, name_1, placeholder_1, type_1, name_2, placeholder_2, type_2, name_3, placeholder_3, type_3, option_0, value_0, option_1, value_1, option_2, value_2, option_3, value_3, button_0, button_type_0, heading0, heading1, label0, label1, label2, label3, label4;
+    export let p, h1, h2, h3, a, input, option, img, button, span, label;
 </script>
 
 <!-- ============================================ -->
@@ -12,22 +12,27 @@
 <section id="cs-contact-486">
 <div class="cs-container">
 <div class="cs-content">
-<h2 class="cs-title">{h2_0}</h2>
-<p class="cs-text">{p_0}</p>
-<span class="cs-heading">{heading0}</span>
-<span class="cs-heading">{heading1}</span>
-<a class="cs-link" href="{href_0}">{a_0}</a>
-<a class="cs-link" href="{href_1}">{a_1}</a>
+<h2 class="cs-title">{h2.h2_0}</h2>
+<p class="cs-text">{p.p_0}</p>
+<span class="cs-heading">{span.heading0}</span>
+<span class="cs-heading">{span.heading1}</span>
+<a class="cs-link" href="{a.href_0attribute}">{a.a_0}</a>
+<a class="cs-link" href="{a.href_1attribute}">{a.a_1}</a>
 </div>
 <!--Form-->
 <form id="cs-form-486" method="post" name="Contact Form">
-<h3 class="cs-h3">{h3_0}</h3>
-<label class="cs-label">{label0}<input class="cs-input" id="name-486" name="{name_0}" placeholder="{placeholder_0}" required="" type="{type_0}"/></label>
-<label class="cs-label">{label1}<input class="cs-input" id="phone-486" name="{name_1}" placeholder="{placeholder_1}" required="" type="{type_1}"/></label>
-<label class="cs-label">{label2}<input class="cs-input" id="email-486" name="{name_2}" placeholder="{placeholder_2}" required="" type="{type_2}"/></label>
-<label class="cs-label">{label3}<input class="cs-input" id="find-486" name="{name_3}" placeholder="{placeholder_3}" type="{type_3}"/></label>
-<label class="cs-label cs-label-message">{label4}</label>
-<button class="cs-button-solid cs-submit" type="{button_type_0}">{button_0}</button>
+<h3 class="cs-h3">{h3.h3_0}</h3>
+<label class="cs-label">{label.label0}<input class="cs-input" id="name-486" name="{input.name_0attribute}" placeholder="{input.placeholder_0attribute}" required="" type="{input.type_0attribute}"/></label>
+<label class="cs-label">{label.label1}<input class="cs-input" id="phone-486" name="{input.name_1attribute}" placeholder="{input.placeholder_1attribute}" required="" type="{input.type_1attribute}"/></label>
+<label class="cs-label">{label.label2}<input class="cs-input" id="email-486" name="{input.name_2attribute}" placeholder="{input.placeholder_2attribute}" required="" type="{input.type_2attribute}"/></label>
+<label class="cs-label">{label.label3}<input class="cs-input" id="find-486" name="{input.name_3attribute}" placeholder="{input.placeholder_3attribute}" type="{input.type_3attribute}"/></label>
+<label class="cs-label cs-label-message">{label.label4}<select id="cs-select-486" name="cars">
+<option value="{option.value_0attribute}">{option.option_0}</option>
+<option value="{option.value_1attribute}">{option.option_1}</option>
+<option value="{option.value_2attribute}">{option.option_2}</option>
+<option value="{option.value_3attribute}">{option.option_3}</option>
+</select></label>
+<button class="cs-button-solid cs-submit" type="{button.type_0attribute}">{button.button_0}</button>
 </form>
 </div>
 </section>
