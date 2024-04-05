93b05eecfe6122754eb1b291ff8b34f8835c3c2f
Rockingham-Web-Services1 <alessandra@rockinghamwebservices.com>
2024-04-06 06:42:22 +0800

    updatred saturday

--- contact_486/contact_486_modified/contact_486_v2.svelte
+++ contact_486/contact_486_modified/contact_486_v2.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let p, h1, h2, h3, a, input, option, img, button, span, label;
+    export let p, h1, h2, h3, a, input, option, img, button, label, svg, span, label;
 </script>
 
 <!-- ============================================ -->
@@ -22,16 +22,11 @@
 <!--Form-->
 <form id="cs-form-486" method="post" name="Contact Form">
 <h3 class="cs-h3">{h3.h3_0}</h3>
-<label class="cs-label">{label.label0}<input class="cs-input" id="name-486" name="{input.name_0attribute}" placeholder="{input.placeholder_0attribute}" required="" type="{input.type_0attribute}"/></label>
-<label class="cs-label">{label.label1}<input class="cs-input" id="phone-486" name="{input.name_1attribute}" placeholder="{input.placeholder_1attribute}" required="" type="{input.type_1attribute}"/></label>
-<label class="cs-label">{label.label2}<input class="cs-input" id="email-486" name="{input.name_2attribute}" placeholder="{input.placeholder_2attribute}" required="" type="{input.type_2attribute}"/></label>
-<label class="cs-label">{label.label3}<input class="cs-input" id="find-486" name="{input.name_3attribute}" placeholder="{input.placeholder_3attribute}" type="{input.type_3attribute}"/></label>
-<label class="cs-label cs-label-message">{label.label4}<select id="cs-select-486" name="cars">
-<option value="{option.value_0attribute}">{option.option_0}</option>
-<option value="{option.value_1attribute}">{option.option_1}</option>
-<option value="{option.value_2attribute}">{option.option_2}</option>
-<option value="{option.value_3attribute}">{option.option_3}</option>
-</select></label>
+<label class="cs-label">{label.label0}</label>
+<label class="cs-label">{label.label1}</label>
+<label class="cs-label">{label.label2}</label>
+<label class="cs-label">{label.label3}</label>
+<label class="cs-label cs-label-message">{label.label4}</label>
 <button class="cs-button-solid cs-submit" type="{button.type_0attribute}">{button.button_0}</button>
 </form>
 </div>
---
+++ plumbing_stitches/cs-navigation-1343/cs-navigation-1343_modified/cs-navigation-1343.svelte
@@ -0,0 +1,72 @@
+<script>
+    export let p, h1, h2, h3, a, input, option, img, button, span, label;
+</script>
+
+<!-- ============================================ -->
+<!--                cs-navigation-1343                  -->
+<!-- ============================================ -->
+
+<!-- ============================================ -->
+<!--                 Navigation                   -->
+<!-- ============================================ -->
+<header id="cs-navigation">
+<div class="cs-top-bar">
+<div class="cs-top-container">
+<!--Nav Logo-->
+<a aria-label="back to home" class="cs-logo" href="{a.href_0attribute}">{a.a_0}</a>
+<div class="cs-contact-group">
+<a class="cs-contact-link" href="{a.href_1attribute}">{a.a_1}</a>
+<a class="cs-contact-link" href="{a.href_2attribute}">{a.a_2}</a>
+<a class="cs-button-solid" href="{a.href_3attribute}">{a.a_3}</a>
+</div>
+<!--Dark Mode toggle, uncomment button code if you want to enable a dark mode toggle-->
+<button aria-label="dark mode toggle" id="dark-mode-toggle">{button.button_0}</button>
+</div>
+</div>
+<!--Wrapped the bottom container in a wrapper div so we can have a full width background color, and centered the container inside of it-->
+<div class="cs-bottom-bar">
+<div class="cs-container">
+<!--Navigation List-->
+<nav class="cs-nav" role="navigation">
+<!--Mobile Nav Toggle-->
+<button aria-label="mobile menu toggle" class="cs-toggle">{button.button_1}</button>
+<!-- We need a wrapper div so we can set a fixed height on the cs-ul in case the nav list gets too long from too many dropdowns being opened and needs to have an overflow scroll. This wrapper acts as the background so it can go the full height of the screen and not cut off any overflowing nav items while the cs-ul stops short of the bottom of the screen, which keeps all nav items in view no matter how mnay there are-->
+<div class="cs-ul-wrapper">
+<ul aria-expanded="false" class="cs-ul" id="cs-expanded">
+<li class="cs-li">
+<a class="cs-li-link cs-active" href="{a.href_4attribute}">{a.a_4}</a>
+</li>
+<li class="cs-li">
+<a class="cs-li-link" href="{a.href_5attribute}">{a.a_5}</a>
+</li>
+<!--Copy and paste this cs-dropdown list item and replace any .cs-li with this cs-dropdown group to make a new dropdown and it will work-->
+<li class="cs-li cs-dropdown" tabindex="0">
+<span class="cs-li-link">
+                                Services
+                                <img alt="{img.alt_1attribute}" aria-hidden="true" class="cs-drop-icon" decoding="async" height="{img.height_1attribute}" src="{img.src_1attribute}" width="{img.width_1attribute}"/>
+</span>
+<ul class="cs-drop-ul">
+<li class="cs-drop-li">
+<a class="cs-li-link cs-drop-link" href="{a.href_6attribute}">{a.a_6}</a>
+</li>
+<li class="cs-drop-li">
+<a class="cs-li-link cs-drop-link" href="{a.href_7attribute}">{a.a_7}</a>
+</li>
+</ul>
+</li>
+<li class="cs-li">
+<a class="cs-li-link" href="{a.href_8attribute}">{a.a_8}</a>
+</li>
+<li class="cs-li">
+<a class="cs-li-link" href="{a.href_9attribute}">{a.a_9}</a>
+</li>
+<li class="cs-li">
+<a class="cs-li-link" href="{a.href_10attribute}">{a.a_10}</a>
+</li>
+</ul>
+</div>
+</nav>
+<a class="cs-call" href="{a.href_11attribute}">{a.a_11}</a>
+</div>
+</div>
+</header>
